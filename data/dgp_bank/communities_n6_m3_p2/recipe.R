43fdb3e449a6430285ccd3a638b5939e68898548
Jesus Pinera Esquivel <jpineraesquivel@gmail.com>
2026-08-24 15:52:19 +0100

    Add Communities DGP data to the bank

---
+++ data/dgp_bank/communities_n6_m3_p2/recipe.R
@@ -0,0 +1,23 @@
+source("R/network_diagnostics.R")
+source("R/utils_network.R")
+source("R/simulate_var_v3.R")
+
+# DGP: communities_n6_m3_p2
+dgp <- eval({
+  {
+      n_units <- 6L
+      m <- 3L
+      p_lags <- 2L
+      make_A_community_block(n_units = n_units, m = m, p_lags = p_lags, 
+          communities = list(c(1L, 2L, 3L), c(4L, 5L, 6L)), self_diag = c(0.46, 
+              0.05), self_off = c(0.34, 0.05)/6, self_diag_sign = 1, 
+          self_off_sign = 1, self_off_pattern = "alternating", 
+          within_diag = c(0.045, 0.027), between_diag = c(0, 0), 
+          within_diag_sign = 1, within_off_sign = 1, between_diag_sign = 1, 
+          between_off_sign = 1, community_off_pattern = "positive", 
+          off_ratio = 0.9, coef_jitter_log_sd = 0.2, jitter_seed = 11L)
+  }
+})
+
+A_list <- dgp$A_list
+meta <- dgp$meta
---
+++ data/dgp_bank/communities_n6_m3_p4/recipe.R
@@ -0,0 +1,23 @@
+source("R/network_diagnostics.R")
+source("R/utils_network.R")
+source("R/simulate_var_v3.R")
+
+# DGP: communities_n6_m3_p4
+dgp <- eval({
+  {
+      n_units <- 6L
+      m <- 3L
+      p_lags <- 4L
+      make_A_community_block(n_units = n_units, m = m, p_lags = p_lags, 
+          communities = list(c(1L, 2L, 3L), c(4L, 5L, 6L)), self_diag = c(0.36, 
+              0.05, 0.02, 0.01), self_off = c(0.35, 0.05, 0.02, 
+              0.01)/6, self_diag_sign = 1, self_off_sign = 1, self_off_pattern = "alternating", 
+          within_diag = c(0.037, 0.032, 0.01, 0.006), between_diag = c(0, 
+              0, 0, 0), within_diag_sign = 1, within_off_sign = 1, 
+          between_diag_sign = 1, between_off_sign = 1, community_off_pattern = "positive", 
+          off_ratio = 0.9, coef_jitter_log_sd = 0.2, jitter_seed = 11L)
+  }
+})
+
+A_list <- dgp$A_list
+meta <- dgp$meta
