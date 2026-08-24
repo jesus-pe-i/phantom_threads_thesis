5e7f6c23c94cb37c55e07a3f6af7a988d8d74e91
Jesus Pinera Esquivel <jpineraesquivel@gmail.com>
2026-08-24 15:46:49 +0100

    Added Block diagonal DGP files

---
+++ data/blockdiag_n6_m3_p3/recipe.R
@@ -0,0 +1,19 @@
+source("R/network_diagnostics.R")
+source("R/utils_network.R")
+source("R/simulate_var_v3.R")
+
+# DGP: blockdiag_n6_m3_p3
+dgp <- eval({
+  {
+      n_units <- 6L
+      m <- 3L
+      p_lags <- 3L
+      make_A_block_diagonal(n_units = n_units, m = m, p_lags = p_lags, 
+          self_diag = c(0.5, 0.09, 0.05), self_off = c(0.5, 0.09, 
+              0.05)/6, self_diag_sign = 1, self_off_sign = 1, self_off_pattern = "alternating", 
+          coef_jitter_log_sd = 0.2, jitter_seed = 11L)
+  }
+})
+
+A_list <- dgp$A_list
+meta <- dgp$meta
